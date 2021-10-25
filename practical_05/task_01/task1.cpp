32acfcbc2754d13f6d20a1cdd89278402de46970
Abhishek-Rana-07 <91174377+Abhishek-Rana-07@users.noreply.github.com>
2021-10-25 12:11:26 +0530

    Add files via upload

---
+++ practical_05/task_01/task1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    float b=10.16;
+    char c='A';
+    bool d=true;
+    long e=1345644;
+    short f=1;
+    double g=4.45456;
+    long double h=345.56845;
+    wchar_t i='B';
+    
+    cout<<"Integer: "<<a<<endl;
+    cout<<"Float: "<<b<<endl;
+    cout<<"Character: "<<c<<endl;
+    cout<<"Boolean: "<<d<<endl;
+    cout<<"Long: "<<e<<endl;
+    cout<<"Short: "<<f<<endl;
+    cout<<"Double: "<<g<<endl;
+    cout<<"Long Double: "<<h<<endl;
+    cout<<"Wide char: "<<i<<endl;
+    
+    return 0;
+}
